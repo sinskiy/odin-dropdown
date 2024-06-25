15cbbe927b83df79712892935a744e55be618dbb
sinskiy <sinskiy@proton.me>
2024-06-25 09:18:12 +0300

    feat: implement basic dropdown

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 import "./reset.css";
 import "./style.css";
+
+const dropdownTriggers = document.querySelectorAll(".dropdown-trigger");
+
+for (const trigger of dropdownTriggers) {
+  trigger.addEventListener("click", () => trigger.classList.toggle("open"));
+}
